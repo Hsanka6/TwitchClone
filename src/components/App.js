8291ad81b1c7a0f1d317ef2f273b7edbd8465511
Hsanka6 <haasith2@gmail.com>
2019-10-21 01:00:38 -0700

    Added routes for all pages and set up header

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,25 @@
 import React from 'react';
-import {BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-  return (
-    <div>
-    Page ONE
-    <Link to="/pagetwo"> Navigate to Page 2 </Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-    Page Two
-    <Link to="/"> Navigate to Page 1 </Link>
-    </div>
-  );
-};
-
+import {BrowserRouter, Route } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
+import StreamCreate from './streams/StreamCreate';
+import Header from './Header';
 
 
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
    </div>
