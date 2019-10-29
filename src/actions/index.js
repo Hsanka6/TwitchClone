dd69ad210da97bb5fa238979b191d654dcb4f403
Hsanka6 <haasith2@gmail.com>
2019-10-29 00:51:57 -0700

    Displays list of streams and added ui for deleting editing streams

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,18 +21,18 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch,getState)=> {
+  const {userId} = getState().auth;
+  const response = await streams.post('/streams', {...formValues,userId});
 
   dispatch ({type: CREATE_STREAM, payload: response.data});
 };
 
-export const fetchStreams =  async dispatch => {
+export const fetchStreams = () => async dispatch => {
   const response = await streams.get('/streams');
 
   dispatch ({type: FETCH_STREAMS, payload: response.data});
 };
-
 
 export const fetchStream = id => async dispatch => {
     const response = await streams.get(`/streams/${id}`);
@@ -44,8 +44,7 @@ export const editStream = (id, formValues) => async dispatch =>{
   dispatch ({type: EDIT_STREAM, payload: response.data});
 };
 
-
-export const deleteStream = (id) => async dispatch =>{
+export const deleteStream = (id) => async dispatch => {
   await streams.delete(`/streams/${id}`);
   dispatch ({type: DELETE_STREAM, payload: id});
 };
