2e328c09037d87cb5048965d7274e540b9750f41
Hsanka6 <haasith2@gmail.com>
2019-10-31 00:37:09 -0700

    Made modal navigate and designed modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Modal from '../Modal';
+const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui primary button negative"> Delete </button>
+      <button className="ui button"> Cancel </button>
+    </React.Fragment>
+  );
+  return (
+    <div>
+    DELETE
+    <Modal
+      title="Delete Stream"
+      content="Are you sure you want to delete this string?"
+      actions={actions}
+     />
+    </div>
+  );
 
-const StreamDelete = () =>{
-  return <div>StreamDelete </div>;
 };
 
 export default StreamDelete;
